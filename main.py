5eca4b2aeaa85a9404d800379f4866a5e8fc572a
Aryan <guptaryan1@outlook.com>
2026-03-13 16:35:55 +0530

    more testing

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from pynput import keyboard
 
-def press(key):
-    print(key)
+def on_press(key):
+    try:
+        if key.char is not None:
+            print(key.char)
+    except AttributeError:
+        pass
 
-with keyboard.Listener(on_press=press) as listener:
+with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
