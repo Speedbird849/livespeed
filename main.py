e6f4912120b025d760c7b368ea0f156b37359d34
Aryan <guptaryan1@outlook.com>
2026-03-13 21:16:21 +0530

    console outputs wpm

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from pynput import keyboard
+from collections import deque
+import time
 
 def on_press(key):
     try:
         if key.char is not None:
-            print(key.char)
+            times.append(time.monotonic())
+            print(calculate_wpm())
     except AttributeError:
         pass
 
+times = deque()
+
+window = 3
+
+def calculate_wpm():
+    now = time.monotonic()
+    cutoff = now - window
+
+    if not times:
+        return 0
+
+    while (times and times[0] < cutoff):
+        times.popleft()
+
+    return int((len(times) / 5) / (window / 60))
+
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
+
+    
